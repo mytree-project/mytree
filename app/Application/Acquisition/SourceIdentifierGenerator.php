59c09fa8ce989e6b4a3cdd9e24d605f1ff84ecef
saulgajda <saulgajda@users.noreply.github.com>
2026-09-05 16:25:00 +0200

    Generate Mention identifiers

--- app/Application/Acquisition/SourceIdentifierGenerator.php
+++ app/Application/Acquisition/SourceIdentifierGenerator.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Application\Acquisition;
 
+use App\Domain\Acquisition\MentionId;
 use App\Domain\Acquisition\SourceAssetId;
 use App\Domain\Acquisition\SourceId;
 use App\Domain\Acquisition\SourceTextId;
@@ -15,4 +16,6 @@ interface SourceIdentifierGenerator
     public function sourceTextId(): SourceTextId;
 
     public function sourceAssetId(): SourceAssetId;
+
+    public function mentionId(): MentionId;
 }
