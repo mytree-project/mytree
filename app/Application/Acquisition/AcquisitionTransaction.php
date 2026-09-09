33bcf0c283ad3f16b89ffbf834ea828216375ac2
saulgajda <saulgajda@users.noreply.github.com>
2026-09-09 07:46:19 +0200

    Fix AcquisitionTransaction PHPDoc formatting

--- app/Application/Acquisition/AcquisitionTransaction.php
+++ app/Application/Acquisition/AcquisitionTransaction.php
@@ -8,7 +8,9 @@ interface AcquisitionTransaction
 {
     /**
      * @template T
+     *
      * @param  callable(): T  $operation
+     *
      * @return T
      */
     public function run(callable $operation): mixed;
