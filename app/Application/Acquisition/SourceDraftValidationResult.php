0159374fe6568a55c240221cc29115425c791838
saulgajda <saulgajda@users.noreply.github.com>
2026-09-12 14:26:00 +0200

    Add SourceDraft validation result

---
+++ app/Application/Acquisition/SourceDraftValidationResult.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Acquisition;
+
+final readonly class SourceDraftValidationResult
+{
+    /** @var list<SourceDraftValidationIssue> */
+    public array $issues;
+
+    /** @param list<SourceDraftValidationIssue> $issues */
+    public function __construct(array $issues = [])
+    {
+        $this->issues = array_values($issues);
+    }
+
+    public function isValid(): bool
+    {
+        foreach ($this->issues as $issue) {
+            if ($issue->severity === SourceDraftValidationIssue::ERROR) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
