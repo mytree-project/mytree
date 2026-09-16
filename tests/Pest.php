d5d0fabce6bd6f0a7aea026afa62224c5726dd97
saulgajda <saulgajda@users.noreply.github.com>
2026-09-16 13:23:03 +0200

    Configure Pest browser tests

---
+++ tests/Pest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+pest()
+    ->extend(TestCase::class)
+    ->use(RefreshDatabase::class)
+    ->in('Browser');
+
+pest()->browser()->timeout(10_000);
