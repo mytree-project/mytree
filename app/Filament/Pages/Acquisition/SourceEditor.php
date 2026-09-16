5f6396651e2e7f0f2b8a2398c7bd237852cdebe9
saulgajda <saulgajda@users.noreply.github.com>
2026-09-16 08:53:28 +0200

    fix: narrow Source workspace width return type

--- app/Filament/Pages/Acquisition/SourceEditor.php
+++ app/Filament/Pages/Acquisition/SourceEditor.php
@@ -15,7 +15,7 @@ use Filament\Support\Enums\Width;
  */
 final class SourceEditor extends SourceWorkspacePage
 {
-    public function getMaxWidth(): Width|string|null
+    public function getMaxWidth(): Width
     {
         return Width::Full;
     }
